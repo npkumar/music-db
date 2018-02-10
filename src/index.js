64d7055d095699385eaefa917d6b68e05962dbf7
Nitin Pradeep Kumar <getnpk@gmail.com>
2018-02-10 10:42:51 +0800

    Fix lot of react version issues

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Home from './components/home';
+import Artist from './components/artist';
 import registerServiceWorker from './registerServiceWorker';
+
+
+const App = () => {
+  
+  return (
+    <BrowserRouter>
+      <div>
+        <Route exact path="/" component={Home}/>
+        <Route path="/artist/:artistId" component={Artist}/>
+      </div>
+    </BrowserRouter>
+  )
+};
+
+App.propTypes = {
+  basename: PropTypes.string.isRequired
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
