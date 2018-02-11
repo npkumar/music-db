1af2e2cef5eb46551640b2de76ac296bda6b8ce3
Nitin Pradeep Kumar <getnpk@gmail.com>
2018-02-11 10:30:02 +0800

    Add show list items

--- src/components/artistList.js
+++ src/components/artistList.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const ArtistList = ({ allArtists }) => {
+const ArtistList = (props) => {
+
+    const list = ({ allArtists }) => {
+      if (allArtists) {
+        return allArtists.map(item => {
+          const style = {
+            background: `url('./images/cover/${item.cover}.jpg') no-repeat`
+          }
+          return (
+            <Link
+              key={item.id}
+              to={`/artist/${item.id}`}
+              className="artist-item"
+              style={style}
+            >
+            <div>{item.name}</div>
+            </Link>
+          )
+        });
+      }
+    }
     return (
-      <div>List</div>
+      <div className="artists-list">
+        <h4>Browse Artists</h4>
+        {list(props)}
+      </div>
     )
 };
 
