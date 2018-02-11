1c2aac4ba49c09bb210781b522cefe9441733289
Nitin Pradeep Kumar <getnpk@gmail.com>
2018-02-11 20:57:28 +0800

    Flesh out artist component

--- src/components/artist.js
+++ src/components/artist.js
@@ -1,10 +1,47 @@
 import React, { Component } from 'react';
+import Header from './header';
+
+const URL = 'http://localhost:3004/artists';
 
 class Artist extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      artist: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch(`${URL}/${this.props.match.params.artistId}`, {
+      method: 'GET'
+    })
+    .then(res => res.json())
+    .then(json => {
+      this.setState({
+        artist: json
+      })
+    });
+  }
+
   render() {
+    const artist = this.state.artist;
     return (
       <div>
-        Artist
+        <Header />
+        <div className="artist-bio">
+          <div className="avatar">
+            <span 
+              style={{ background:`url('/images/covers/${artist.cover}.jpg') no-repeat`}}>
+            </span>
+          </div>
+          <div className="bio">
+            <h3>{artist.name}</h3>
+            <div className="bio-text">
+              {artist.bio}
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
