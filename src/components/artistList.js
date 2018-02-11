7f94cfdc2382e515a919732a970002a88c333b15
Nitin Pradeep Kumar <getnpk@gmail.com>
2018-02-11 10:18:20 +0800

    Add stateless ArtistList component

---
+++ src/components/artistList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ArtistList = ({ allArtists }) => {
+    return (
+      <div>List</div>
+    )
+};
+
+export default ArtistList;
--- src/components/home.js
+++ src/components/home.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
 
 import Banner from './banner.js';
+import ArtistList from './artistList';
+
+const URL_ARTISTS = 'http://localhost:3004/artists';
 
 class Home extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      artists: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch(URL_ARTISTS, {
+      method: 'GET'
+    })
+    .then(res => res.json())
+    .then(json => {
+      this.setState({
+        artists: json
+      });
+    })
+  }
+
   render() {
     return (
       <div>
         <Banner />
-        Home
+        <ArtistList allArtists={this.state.artists}/>
       </div>
     )
   }
